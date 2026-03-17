42d405768caddb0bab985c6d5be8d0b368f8d2d6
Herik25 <harshparmar87990@gmail.com>
2026-03-17 21:55:32 +0530

    added shadows

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ const render = new THREE.WebGLRenderer({ antialias: true });
 render.setPixelRatio(window.devicePixelRatio);
 render.setSize(window.innerWidth, window.innerHeight);
 render.setClearColor(0x87ceeb, 1);
+render.shadowMap.enabled = true;
 document.body.appendChild(render.domElement);
 
 // Camera Setup
@@ -41,13 +42,20 @@ scene.add(world);
 
 // Lighting
 function setUpLighting() {
-  const light1 = new THREE.DirectionalLight();
-  light1.position.set(1, 1, 1);
-  scene.add(light1);
+  const sun = new THREE.DirectionalLight();
+  sun.position.set(50, 50, 50);
+  sun.castShadow = true;
+  sun.shadow.camera.near = 0.1;
+  sun.shadow.camera.far = 100;
+  sun.shadow.camera.left = -50;
+  sun.shadow.camera.right = 50;
+  sun.shadow.camera.top = 50;
+  sun.shadow.camera.bottom = -50;
+  sun.shadow.bias = -0.001;
+  scene.add(sun);
 
-  const light2 = new THREE.DirectionalLight();
-  light2.position.set(-1, -1, -0.5);
-  scene.add(light2);
+  // const shadowHelper = new THREE.CameraHelper(sun.shadow.camera);
+  // scene.add(shadowHelper);
 
   const ambientLight = new THREE.AmbientLight();
   ambientLight.intensity = 0.5;
